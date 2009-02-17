4f8da3efbac5989df980df918457cc4ad36b5b6d
Zbigniew Łukasiak <zbigniew@lukasiak.name>
2009-02-17 09:53:11 +0000

    Import of ZBY/Rose-HTMLx-Form-DBIC-0.02 from CPAN.
    gitpan-cpan-distribution: Rose-HTMLx-Form-DBIC
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ZBY/Rose-HTMLx-Form-DBIC-0.02.tar.gz
    gitpan-cpan-author:       ZBY
    gitpan-cpan-maturity:     released

--- lib/Rose/HTMLx/Form/DBIC.pm
+++ lib/Rose/HTMLx/Form/DBIC.pm
@@ -8,7 +8,7 @@ use Moose;
 BEGIN {
     use Exporter ();
     use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-    $VERSION     = '0.01';
+    $VERSION     = '0.02';
 #    @ISA         = qw(Exporter);
     #Give a hoot don't pollute, do not export more than needed by default
 #    @EXPORT      = qw( );
